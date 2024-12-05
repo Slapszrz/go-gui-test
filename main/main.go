324defec967c60ba2da067ec49571455fc45fd73
Slapszrz <simon.martin.akmk@gmail.com>
2024-12-05 17:44:42 +0100

    Button alignment + refactoring

--- main/main.go
+++ main/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"gioui.org/app"
+	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+	"log"
+	"os"
 )
 
 func main() {
@@ -18,34 +21,57 @@ func main() {
 		w.Option(app.Size(unit.Dp(count), unit.Dp(count)))
 		//w.Option(app.MinSize(unit.Dp(0), unit.Dp(0)))
 
-		var ops op.Ops
-
-		var startButton widget.Clickable
-
-		theme := material.NewTheme()
-
-		// listen for events in the window.
-		for {
-			// first grab the event
-			evt := w.Event()
-
-			// then detect the type
-			switch typ := evt.(type) {
-			// re-render
-			case app.FrameEvent:
-				gtx := app.NewContext(&ops, typ)
-
-				btn := material.Button(theme, &startButton, "Start")
-
-				btn.Layout(gtx)
-				typ.Frame(gtx.Ops)
-
-			// exit event
-			case app.DestroyEvent:
-				count -= 10
-				w.Option(app.Size(unit.Dp(count), unit.Dp(count)))
-			}
+		if err := draw(w); err != nil {
+			log.Fatal(err)
 		}
+
+		os.Exit(0)
 	}()
 	app.Main()
+}
+
+type C = layout.Context
+type D = layout.Dimensions
+
+func draw(w *app.Window) error {
+
+	var ops op.Ops
+
+	var startButton widget.Clickable
+
+	theme := material.NewTheme()
+
+	// listen for events in the window.
+	for {
+		// first grab the event
+
+		// then detect the type
+		switch e := w.Event().(type) {
+		// re-render
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+
+			layout.Flex{
+				Axis:    layout.Vertical,
+				Spacing: layout.SpaceStart,
+			}.Layout(gtx,
+
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						btn := material.Button(theme, &startButton, "Start")
+
+						return btn.Layout(gtx)
+					}),
+				layout.Rigid(
+					layout.Spacer{Height: unit.Dp(25)}.Layout,
+				),
+			)
+
+			e.Frame(gtx.Ops)
+
+		// exit event
+		case app.DestroyEvent:
+			return e.Err
+		}
+	}
 }
